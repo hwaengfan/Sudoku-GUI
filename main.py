3aaa5f6e735abf858d645d4a1b501bdab95073cd
Hoang <21hoangphan@gmail.com>
2022-06-04 15:18:07 -0700

    update input numbers statements

--- main.py
+++ main.py
@@ -29,35 +29,34 @@ def main():
 
             # Input numbers
             if event.type == pygame.KEYDOWN:
-                match event.key:
-                    case pygame.K_1 | pygame.K_KP1:
-                        key = 1
-                    case pygame.K_2 | pygame.K_KP2:
-                        key = 2
-                    case pygame.K_3 | pygame.K_KP3:
-                        key = 3
-                    case pygame.K_4 | pygame.K_KP4:
-                        key = 4
-                    case pygame.K_5 | pygame.K_KP5:
-                        key = 5
-                    case pygame.K_6 | pygame.K_KP6:
-                        key = 6
-                    case pygame.K_7 | pygame.K_KP7:
-                        key = 7
-                    case pygame.K_8 | pygame.K_KP8:
-                        key = 8
-                    case pygame.K_9 | pygame.K_KP9:
-                        key = 9
-                    case pygame.K_RETURN:
-                        i, j = board.selectedPos
-                        currCube = board.cubes[i][j]
-                        if board.cubes[i][j].temp != 0:
-                            if not board.replaceTemp(currCube.temp):
-                                strikes += 1
-                                # Check if game is over
-                                if strikes == 3:
-                                    run = False
-                            key = None
+                if event.key == pygame.K_1 or event.key == pygame.K_KP1:
+                    key = 1
+                if event.key == pygame.K_2 or event.key == pygame.K_KP2:
+                    key = 2
+                if event.key == pygame.K_3 or event.key == pygame.K_KP3:
+                    key = 3
+                if event.key == pygame.K_4 or event.key == pygame.K_KP4:
+                    key = 4
+                if event.key == pygame.K_5 or event.key == pygame.K_KP5:
+                    key = 5
+                if event.key == pygame.K_6 or event.key == pygame.K_KP6:
+                    key = 6
+                if event.key == pygame.K_7 or event.key == pygame.K_KP7:
+                    key = 7
+                if event.key == pygame.K_8 or event.key == pygame.K_KP8:
+                    key = 8
+                if event.key == pygame.K_9 or event.key == pygame.K_KP9:
+                    key = 9
+                if event.key == pygame.K_RETURN:
+                    i, j = board.selectedPos
+                    currCube = board.cubes[i][j]
+                    if board.cubes[i][j].temp != 0:
+                        if not board.replaceTemp(currCube.temp):
+                            strikes += 1
+                            # Check if game is over
+                            if strikes == 3:
+                                run = False
+                        key = None
 
                         if board.isFinished():
                             print("Game over")
